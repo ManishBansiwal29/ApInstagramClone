166cfe5f548323eceb0cad63b417a8f004988a02
ManishBansiwal29 <manishbansiwal29@gmail.com>
2020-01-18 01:01:40 +0530

    sign up and login parse

--- app/src/main/java/com/example/apinstagramclone/SignUpLoginActivity.java
+++ app/src/main/java/com/example/apinstagramclone/SignUpLoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.apinstagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +43,8 @@ public class SignUpLoginActivity extends AppCompatActivity {
                         if(e == null){
                             FancyToast.makeText(SignUpLoginActivity.this,
                                     appUser.get("username")+" saved successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                            Intent intent = new Intent(SignUpLoginActivity.this,WelcomeActivity.class);
+                            startActivity(intent);
                         }else {
                             FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(),
                                     FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
@@ -61,6 +64,8 @@ public class SignUpLoginActivity extends AppCompatActivity {
                         if(user != null && e == null){
                             FancyToast.makeText(SignUpLoginActivity.this,
                                     user.get("username") + " is logged in successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                            Intent intent = new Intent(SignUpLoginActivity.this,WelcomeActivity.class);
+                            startActivity(intent);
                         }
                         else {
                             FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(),
